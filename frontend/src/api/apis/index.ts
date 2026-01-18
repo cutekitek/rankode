e0311c47bd4208670fb7a7824e9d75ff03cf882c
Andrey <andrtazet@gmail.com>
2026-01-18 11:52:51 +0300

    remove old frontend

--- frontend/src/api/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AttemptsApi';
-export * from './AuthApi';
-export * from './TasksApi';
-export * from './TestCasesApi';
-export * from './TopicsApi';
-export * from './UsersApi';
